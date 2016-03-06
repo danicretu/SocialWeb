8e2993a63c04d628d2081cb195b4f23fdabcb594
zloco <aploco1@etf.unsa.ba>
2016-03-07 00:44:41 +0100

    edited my profile page

--- socialwebproject/networkinstitute/models.py
+++ socialwebproject/networkinstitute/models.py
@@ -4,13 +4,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+PROJECT_STATUS_CHOICES = (
+	('O', 'Operative'),
+	('A', 'Accepted'),
+	('D', 'Declined')
+)
+
 class Member(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 	facebook = models.CharField(max_length=100, blank=True)
 	twitter = models.CharField(max_length=100, blank=True)
 
-	#def __str__(self):
-	#	return "{0} vs {1}".format(self.first_player, self.second_player)
+	def __str__(self):
+		return "{0}".format(self.user.get_username())
 
 class ProjectOwner(models.Model):
 	member = models.OneToOneField(Member, on_delete=models.CASCADE)
@@ -21,6 +27,8 @@ class Project(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.TextField()
 	deadline = models.DateField()
+	status = models.CharField(max_length=1, default='O',
+		choices=PROJECT_STATUS_CHOICES)
 
 class Faculty(models.Model):
 	project = models.ForeignKey(Project)
--- socialwebproject/userprofile/views.py
+++ socialwebproject/userprofile/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from networkinstitute.models import Member
 
 # Create your views here.
+@login_required
 def home(request):
-	return render(request, "userprofile/home.html")
+	member = Member.objects.get(user=request.user)
+	full_name = member.user.get_full_name()
+	email = member.user.email
+	facebook = member.facebook
+	twitter = member.twitter
+	context = {'full_name': full_name,
+				'email': email,
+				'facebook': facebook,
+				'twitter': twitter}
+	return render(request, "userprofile/home.html", context)
