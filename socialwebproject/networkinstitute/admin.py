901459f9968a027bdbdc1ddc24bbe5d5a557a6f9
zloco <aploco1@etf.unsa.ba>
2016-03-06 17:55:28 +0100

    edited admin.py

--- socialwebproject/networkinstitute/admin.py
+++ socialwebproject/networkinstitute/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Member, ProjectOwner, Project, Faculty
 
 # Register your models here.
+admin.site.register(Member)
+admin.site.register(ProjectOwner)
+admin.site.register(Project)
+admin.site.register(Faculty)
--- socialwebproject/socialwebproject/urls.py
+++ socialwebproject/socialwebproject/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', admin.site.urls),
