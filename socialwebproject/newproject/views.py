db38769e20e808098e7621e6bd6a7977f3a8e3e4
zloco <aploco1@etf.unsa.ba>
2016-03-07 22:06:47 +0100

    removed hidden input

--- socialwebproject/newproject/views.py
+++ socialwebproject/newproject/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.db import connection, transaction
 
-from networkinstitute.models import Project
+from networkinstitute.models import Member
 from networkinstitute.models import ProjectOwner
+from networkinstitute.models import Project
+
 from .forms import NewProjectForm
 
 # Create your views here.
 @login_required
 def home(request):
 	if request.method == 'POST':
-		#member = request.user
+		userid = request.user.id
+		#cursor = connection.cursor()
+		#cursor.execute("SELECT * FROM Members WHERE id = %s", [userid])
+		#member = cursor.fetchone()
 		form = NewProjectForm(data=request.POST)
 		if form.is_valid():
 			form.save()
