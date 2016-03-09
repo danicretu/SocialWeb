574982f999db2e453995b64ad8e755592661a541
zloco <aploco1@etf.unsa.ba>
2016-03-09 23:18:32 +0100

    added myownedprojects page

--- socialwebproject/myownedprojects/views.py
+++ socialwebproject/myownedprojects/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from networkinstitute.models import CustomUser, ProjectOwner, Project, Faculty
+
 # Create your views here.
 @login_required
 def home(request):
-	return render(request, "myownedprojects/home.html")
+	member = request.user
+	owners = ProjectOwner.objects.all()
+	for o in owners:
+		if o.member_id == member.id:
+			owner = o
+			break
+	projects = Project.objects.filter(owner=owner)
+	context = {'projects': projects}
+	return render(request, "myownedprojects/home.html", context)
