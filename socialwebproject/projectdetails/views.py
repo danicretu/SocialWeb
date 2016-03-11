38cbcb060a5d5f0f5ab2ce72a4ce8cecf6b2ba98
zloco <aploco1@etf.unsa.ba>
2016-03-11 17:59:42 +0100

    users can apply for projects

--- socialwebproject/projectdetails/views.py
+++ socialwebproject/projectdetails/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from networkinstitute.models import CustomUser, ProjectOwner, Project, Faculty
 
 # Create your views here.
@@ -8,4 +9,10 @@ from networkinstitute.models import CustomUser, ProjectOwner, Project, Faculty
 @login_required
 def home(request, pk):
     project = get_object_or_404(Project, pk=pk)
+    if request.method == "POST":
+        member = request.user
+        project = Project.objects.get(pk=pk)
+        project.members.add(member)
+        project.save()
+        messages.success(request, 'You have successfully applied for this project!')
     return render(request, 'projectdetails/home.html', {'project': project})
