8a64bb63d963120003f5783479575befdb5c069f
zloco <aploco1@etf.unsa.ba>
2016-03-12 14:15:02 +0100

    user can now see applied projects

--- socialwebproject/myappliedprojects/views.py
+++ socialwebproject/myappliedprojects/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from networkinstitute.models import CustomUser, ProjectOwner, Project, Faculty
+
 # Create your views here.
 @login_required
 def home(request):
+	member = request.user
+	projects = list()
+	for p in Project.objects.all():
+		for m in p.members.all():
+			if m == member and p.status == 'O':
+				projects.append(p)
+	if projects.count > 0:
+		context = {'projects': projects}
+		return render(request, "myappliedprojects/home.html", context)
 	return render(request, "myappliedprojects/home.html")
