5e0876c0980dce5be2cf157e1a6461d0c6fad1cc
zloco <aploco1@etf.unsa.ba>
2016-03-13 12:25:59 +0100

    enabled approving and declining users

--- socialwebproject/socialwebproject/settings.py
+++ socialwebproject/socialwebproject/settings.py
@@ -114,6 +114,39 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'userapproval': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
--- socialwebproject/userapproval/views.py
+++ socialwebproject/userapproval/views.py
@@ -6,31 +6,47 @@ from django.views.decorators.csrf import csrf_protect
 from networkinstitute.models import CustomUser, ProjectOwner, Project, Faculty, Status
 
 # Create your views here.
+import logging
+logger = logging.getLogger(__name__)
+
+def myfunction():
+    logger.debug("this is a debug message!")
+
+def myotherfunction():
+    logger.error("this is an error message!!")
 
 @csrf_protect
 @login_required
 def home(request, pk):
     project = get_object_or_404(Project, pk=pk)
     members = project.members.all()
+    appr = Status.objects.filter(status='A')
+    decl = Status.objects.filter(status='D')
 
     if request.method == "POST":
         for m in members:
             approve = "approve"+str(m.pk)
             decline = "decline"+str(m.pk)
-        if request.POST.get(approve):
-            s = Status.objects.get(project=project, member=m)
-            s.status = 'A'
-            #s.save()
-            messages.success(request, 'You have approved the user!')
-            context = {'project': project}
-            return render(request, 'userapproval/home.html', context)
-        if request.POST.get(decline):
-            s = Status.objects.get(project=project, member=m)
-            s.status = 'D'
-            #s.save()
-            messages.success(request, 'You have declined the user!')
-            context = {'project': project,}
-            return render(request, 'userapproval/home.html', context)
+            if request.POST.get(approve):
+                pk = int(approve[7:])
+                member = CustomUser.objects.get(pk=pk)
+                s = Status.objects.get(project=project, member=member)
+                s.status = 'A'
+                s.save()
+                appr = Status.objects.filter(status='A')
+                messages.success(request, 'You have approved the user!')
+                context = {'project': project, 'appr': appr, 'decl': decl}
+                return render(request, 'userapproval/home.html', context)
+            if request.POST.get(decline):
+                pk = int(decline[7:])
+                member = CustomUser.objects.get(pk=pk)
+                s = Status.objects.get(project=project, member=member)
+                s.status = 'D'
+                s.save()
+                decl = Status.objects.filter(status='D')
+                messages.success(request, 'You have declined the user!')
+                context = {'project': project, 'appr': appr, 'decl': decl}
+                return render(request, 'userapproval/home.html', context)
 
-    context = {'project': project}
+    context = {'project': project, 'appr': appr, 'decl': decl}
     return render(request, 'userapproval/home.html', context)
