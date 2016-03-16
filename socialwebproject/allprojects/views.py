5b3768571c0c9b68f0e4324bc897177036413770
Jovana Jevtic <jovana.jevtic92@gmail.com>
2016-03-16 18:52:46 +0100

    sort buttons views.py

--- socialwebproject/allprojects/views.py
+++ socialwebproject/allprojects/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils import timezone
+
 from django.contrib.auth.decorators import login_required
 
 from networkinstitute.models import CustomUser, ProjectOwner, Project, Faculty
@@ -8,14 +10,64 @@ from networkinstitute.models import CustomUser, ProjectOwner, Project, Faculty
 def home(request):
 	member = request.user
 	owners = ProjectOwner.objects.all()
-	if owners.count > 0:
-		for o in owners:
-			if o.member_id == member.id:
-				owner = o
-				projects = Project.objects.exclude(owner=owner)
-				break
-			else:
-				projects = Project.objects.all()
-		context = {'projects': projects}
-		return render(request, "allprojects/home.html", context)
-	return render(request, "allprojects/home.html")
+	for o in owners:
+		if o.member_id == member.id:
+			owner = o
+			projects = Project.objects.exclude(owner=owner)
+			break
+		else:
+			projects = Project.objects.all()
+	context = {'projects': projects}
+	return render(request, "allprojects/home.html", context)
+
+def deadlinedown(request):
+	member = request.user
+	owners = ProjectOwner.objects.all()
+	for o in owners:
+		if o.member_id == member.id:
+			owner = o
+			projects = Project.objects.exclude(owner=owner)
+			break
+		else:
+			projects = Project.objects.order_by('-deadline')
+	context = {'projects': projects}
+	return render(request, "allprojects/home.html", context)
+
+def deadlineup(request):
+	member = request.user
+	owners = ProjectOwner.objects.all()
+	for o in owners:
+		if o.member_id == member.id:
+			owner = o
+			projects = Project.objects.exclude(owner=owner)
+			break
+		else:
+			projects = Project.objects.order_by('deadline')
+	context = {'projects': projects}
+	return render(request, "allprojects/home.html", context)
+
+def az(request):
+	member = request.user
+	owners = ProjectOwner.objects.all()
+	for o in owners:
+		if o.member_id == member.id:
+			owner = o
+			projects = Project.objects.exclude(owner=owner)
+			break
+		else:
+			projects = Project.objects.order_by('name')
+	context = {'projects': projects}
+	return render(request, "allprojects/home.html", context)
+
+def za(request):
+	member = request.user
+	owners = ProjectOwner.objects.all()
+	for o in owners:
+		if o.member_id == member.id:
+			owner = o
+			projects = Project.objects.exclude(owner=owner)
+			break
+		else:
+			projects = Project.objects.order_by('-name')
+	context = {'projects': projects}
+	return render(request, "allprojects/home.html", context)
